ac57a7b84fe6729a173fdcb17aa8757fde25b487
3dvd3 <73511429+3dvd3@users.noreply.github.com>
2021-10-25 23:31:19 +0300

    new block update

--- Obshaga.py
+++ Obshaga.py
@@ -11,9 +11,13 @@ from aiogram.utils import executor
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
-from Config import BOT_TOKEN
-from definitions import definitions
-from definitions import harder
+from config import BOT_TOKEN
+
+from questions_human import definitions
+from questions_human import harder
+
+from questions_economy import deffi
+from questions_economy import hards
 
 bot = Bot(token = BOT_TOKEN)
 
@@ -26,7 +30,7 @@ logging.basicConfig(level=logging.ERROR)
 # default commands
 @dp.message_handler(commands = "start")
 async def start(message: types.Message):
-    await bot.send_message(message.chat.id, text = "Привет, я бот для подготовки к ЕГЭ по обществознанию!\n\nДля начала выберите блок задании", reply_markup=kb.makup_theme)
+    await bot.send_message(message.chat.id, text = "Привет, я бот для подготовки к ЕГЭ по обществознанию!\n\nДля начала выберите блок задании⬇.\nИ ознакомьтесь с правилами /help", reply_markup=kb.makup_theme)
     file = r"C:\Users\Totosa\Desktop\obshaga\NameID.txt"
     k = message.from_user.id
     name = message.from_user.username
@@ -36,6 +40,10 @@ async def start(message: types.Message):
     uniqlines = set(open(file,'r', encoding='utf-8').readlines())
     gotovo = open(file,'w', encoding='utf-8').writelines(set(uniqlines))
 
+
+@dp.message_handler(commands = "help")
+async def help(message: types.Message):
+    await bot.send_message(message.chat.id, text = "Управление ботом осущетсвляется при помощи меню снизу⬇.\n\nОтветы на вопросы необходимо просто написать в чат.\nПриятного обучения.✨")
 
 
 
@@ -47,55 +55,6 @@ async def info(message: types.Message):
     c = "".join(contents)
     await message.reply("Все, кто писал боту:\n" + str(c))
 # default commands
-
-
-# reply - question commands
-@dp.message_handler(commands = "d")
-async def defin(message: types.Message):
-    global word
-    global word_next
-    global defin
-    defin = random.choice(definitions)
-    word = defin.split("-")
-    sasuke = word[1]
-    word_next = sasuke.strip()
-    ze_waaardo = word[0]
-    print(str(ze_waaardo) + "это...")
-    print(str(defin))
-    await bot.send_message(message.chat.id, text = ze_waaardo + "это..." + "\n\nЧтобы дать ответ напишите /a *ответ*.")
-
-@dp.message_handler(commands = "a")
-async def answer(message: types.Message):
-    ans = message.text.split("/a")
-    answer = ans[1]
-    print(answer.strip())
-    a = fuzz.token_sort_ratio(answer, word_next)
-    await message.reply("Ваше определение схоже с моим на " + str(a) + " %", reply_markup=kb.makup_return)
-
-@dp.message_handler(commands = "p")
-async def hard(message: types.Message):
-    global hard
-    global hard_words_next
-    global hard_words
-    hard = random.choice(harder)
-    hard_words = hard.split(":")
-    naruto = hard_words[1]
-    hard_words_next = naruto.strip()
-    jojo = hard_words[0]
-    print("Назовите " + str(jojo))
-    print(hard)
-    await bot.send_message(message.chat.id, text = "Назовите " + str(jojo) + ":" + "\n\nЧтобы дать ответ напишите /ap *ответ*.")
-
-@dp.message_handler(commands = "ap")
-async def hard_answer(message: types.Message):
-    ans = message.text.split("/ap")
-    answer = ans[1]
-    print(answer.strip())
-    a = fuzz.token_sort_ratio(answer, hard_words_next)
-    await message.reply("Ваше определение схоже с моим на " + str(a) + " %", reply_markup=kb.makup_hard_return)
-# reply - question commands
-
-
 
 # keyboards reply
 @dp.message_handler(lambda message: message.text == "Выбрать другой блок.")
@@ -123,6 +82,8 @@ async def button_defin(message: types.Message):
     global word
     global word_next
     global defin
+    global mode
+    mode = 1
     defin = random.choice(definitions)
     word = defin.split("-")
     sasuke = word[1]
@@ -130,7 +91,7 @@ async def button_defin(message: types.Message):
     ze_waaardo = word[0]
     print(str(ze_waaardo) + "это...")
     print(str(defin))
-    await bot.send_message(message.chat.id, text = ze_waaardo + "это..." + "\n\nЧтобы дать ответ напишите /a *ответ*.")
+    await bot.send_message(message.chat.id, text = ze_waaardo + "это..." + "\n\nЧтобы дать ответ просто напишите его в чат.")
 
 
 @dp.message_handler(lambda message: message.text == "Сложные вопросы по теме \"Человек и общество\"")
@@ -138,6 +99,8 @@ async def button_hard_quest(message: types.Message):
     global hard
     global hard_words_next
     global hard_words
+    global mode
+    mode = 2
     hard = random.choice(harder)
     hard_words = hard.split(":")
     naruto = hard_words[1]
@@ -145,7 +108,7 @@ async def button_hard_quest(message: types.Message):
     jojo = hard_words[0]
     print("Назовите " + str(jojo))
     print(hard)
-    await bot.send_message(message.chat.id, text = "Назовите " + str(jojo) + ":" + "\n\nЧтобы дать ответ напишите /ap *ответ*.")
+    await bot.send_message(message.chat.id, text = "Назовите " + str(jojo) + ":" + "\n\nЧтобы дать ответ просто напишите его в чат.")
 
 
 
@@ -158,6 +121,93 @@ async def give(message: types.Message):
 async def hard_give(message: types.Message):
     await bot.send_message(message.chat.id, text = str(hard.capitalize()), reply_markup=kb.makup_begin)
 # keyboards reply 
+
+
+@dp.message_handler(lambda message: message.text == "Экономика")
+async def economic(message: types.Message):
+    await bot.send_message(message.chat.id, text = "Вопросы будут пополняться со временем.", reply_markup=kb.makup_choice_econom)
+
+
+@dp.message_handler(lambda message: message.text == "Определения по теме \"Экономика\"")
+async def deffins_econom(message: types.Message):
+    global economic # то про чо надо спросить
+    global econom
+    global true_econom # ответ
+    global mode
+    mode = 3
+    econom = random.choice(deffi)
+    econom_next = econom.split("-")
+    true_econom = econom_next[1]
+    economic = econom_next[0].lstrip()
+    print(economic + "это...")
+    print(econom)
+    await bot.send_message(message.chat.id, text = str(economic) + "это...\n\nЧтобы дать ответ просто напишите его в чат.")
+
+@dp.message_handler(lambda message: message.text == "Сложные вопросы по теме \"Экономика\"")
+async def harder_econom(message: types.Message):
+    global hard_econom
+    global true_hard
+    global hardec
+    global mode
+    mode = 4 
+    hardec = random.choice(hards)
+    hard_eco_next = hardec.split(":")
+    true_hard = hard_eco_next[1]
+    hard_econom = hard_eco_next[0].lstrip()
+    print(hard_econom)
+    print(hardec.capitalize())
+    await bot.send_message(message.chat.id, text = "Назовите " + str(hard_econom) + ":" + "\n\nЧтобы дать ответ просто напишите его в чат.")
+    
+
+@dp.message_handler(lambda message: message.text == "Посмотреть ответ✅")
+async def give(message: types.Message):
+    await bot.send_message(message.chat.id, text = str(econom), reply_markup=kb.makup_choice_econom)
+
+
+@dp.message_handler(lambda message: message.text == "Посмотреть ответ ✅")
+async def hard_give(message: types.Message):
+    await bot.send_message(message.chat.id, text = str(hardec.capitalize()), reply_markup=kb.makup_choice_econom)
+
+
+
+
+
+
+
+
+
+@dp.message_handler(lambda message: message.text == "Политика")
+async def politic(message: types.Message):
+    await bot.send_message(message.chat.id, text = "Вы такое ещё не проходили.", reply_markup=kb.makup_block)
+
+
+@dp.message_handler(lambda message: message.text == "Право")
+async def pravo(message: types.Message):
+    await bot.send_message(message.chat.id, text = "Вы такое ещё не проходили.", reply_markup=kb.makup_block)
+ 
+
+
+
+
+@dp.message_handler(lambda message: message.text.lower())
+async def suka(message: types.Message):
+    r = message.text
+    if mode == 1:
+        a = fuzz.token_sort_ratio(r, word_next)
+        await message.reply("Ваше определение схоже с моим на " + str(a) + " %", reply_markup=kb.makup_return)
+    elif mode == 2:
+        a = fuzz.token_sort_ratio(r, hard_words_next)
+        await message.reply("Ваше определение схоже с моим на " + str(a) + " %", reply_markup=kb.makup_hard_return)
+    elif mode == 3:
+        a = fuzz.token_sort_ratio(r, true_econom)
+        await message.reply("Ваше определение схоже с моим на " + str(a) + " %", reply_markup=kb.makup_returns)
+    elif mode == 4:
+        a = fuzz.token_sort_ratio(r, true_hard)
+        await message.reply("Ваше определение схоже с моим на " + str(a) + " %", reply_markup=kb.makup_returns_hard)
+
+
+
+
 
 
 
--- keyboards.py
+++ keyboards.py
@@ -14,13 +14,21 @@ makup_return = ReplyKeyboardMarkup(
 ).row(another_markup)
 
 
+makup_block = ReplyKeyboardMarkup(
+    resize_keyboard=True, 
+    one_time_keyboard=True,
+).row(another_markup
+)
+
+
+
 return_hard_button = KeyboardButton("Посмотреть правильный ответ✅")
 makup_hard_return = ReplyKeyboardMarkup(
     resize_keyboard=True,
     one_time_keyboard=True,
 ).row(return_hard_button
 ).row(new_defin, new_hard_quest
-)        
+).row(another_markup)        
 
 
 defin_button = KeyboardButton("Определения по теме \"Человек и общество\"")
@@ -41,10 +49,40 @@ makup_begin_human = ReplyKeyboardMarkup(
 
 
 human_and_obshaga = KeyboardButton("Человек и общество")
+economic = KeyboardButton("Экономика")
+politic = KeyboardButton("Политика")
+pravo = KeyboardButton("Право")
 makup_theme = ReplyKeyboardMarkup(
     resize_keyboard=True,
     one_time_keyboard=True,
-).row(human_and_obshaga
-)
+).row(human_and_obshaga, politic
+).row(economic, pravo)
 
 
+
+deffins_economic = KeyboardButton("Определения по теме \"Экономика\"")
+hards_economic = KeyboardButton("Сложные вопросы по теме \"Экономика\"")
+another_markup = KeyboardButton("Выбрать другой блок.")
+makup_choice_econom = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True,
+).row(deffins_economic, hards_economic
+).row(another_markup)
+
+
+return_button_econom = KeyboardButton("Посмотреть ответ✅")
+another_markup = KeyboardButton("Выбрать другой блок.")
+makup_returns = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True,
+).row(return_button_econom
+).row(deffins_economic, hards_economic
+).row(another_markup)
+
+return_button_econom_hard = KeyboardButton("Посмотреть ответ ✅")
+makup_returns_hard = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True,
+).row(return_button_econom_hard
+).row(deffins_economic, hards_economic
+).row(another_markup)
