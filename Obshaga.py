e328b5b86e97f2aed1a7d740ac1f5293e375e8dd
3dvd3 <73511429+3dvd3@users.noreply.github.com>
2021-10-26 17:57:27 +0300

    3213213

--- Obshaga.py
+++ Obshaga.py
@@ -61,7 +61,8 @@ async def ege(message: types.Message):
 
 @dp.message_handler(commands = "users")
 async def info(message: types.Message):
-    k = open("ПК ЕГЭLAND - Мораль.mp3", "r", encoding="UTF-8")
+    fille = r"C:\Users\Totosa\Desktop\obshaga\int.mp3"
+    k = open(fille, "r", encoding="UTF-8")
     contents = k.readlines()
     c = "".join(contents)
     await message.reply("Все, кто писал боту:\n" + str(c))
